f92d6b5f8e869744076ce6fd31adbd78730d27b0
David Silva Troya <davidsiltroy2008@hotmail.com>
2022-12-04 11:20:33 +0100

    Another day in the paradise! lets start! *_*

---
+++ main.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+st.header('Thomas More - Big Data Team Assigment: Image Classification')
+
+st.write("Here is going to be an Epic/Awesome image classificatior program!")
+
+body = st.container()
+body.write("Coming soon.... (in Winter)")
+
