4a4afed79e4abc38c67934082c089b2e42aed299
David Silva Troya <davidsiltroy2008@hotmail.com>
2022-12-05 15:35:38 +0100

    I promess from now on I will use a new branch

---
+++ about_us.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+
+def about_us():
+    body = st.container()
+    body.write("Coming soon.... (in Winter)")
--- main.py
+++ main.py
@@ -1,27 +1,8 @@
 #https://davidsiltroy-python-big-data-image-classification-main-op94jq.streamlit.app/
 import streamlit as st
-from streamlit_option_menu import option_menu
+from navbar import *
 
 
-
-
-#1. as slidebar menu
-with st.sidebar:
-    selected = option_menu(
-        menu_title= "Team 1: JAD Solutions",
-        options = ["Notebook", "IDE", "About us"]
-    )
-
-if selected == "Notebook":
-    st.header('Thomas More - Big Data Team Assigment: Image Classification')
-
-if selected == "IDE":
-    st.write("Here is going to be an Epic/Awesome image classificatior program!")
-
-if selected == "About us":
-    body = st.container()
-    body.write("Coming soon.... (in Winter)")
-
-
-
+if __name__=='__main__':
+    navbar()
 
