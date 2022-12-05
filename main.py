fda4993d1b9d07ca3d7b74c9464a3b3aaf5dd2b6
David Silva Troya <davidsiltroy2008@hotmail.com>
2022-12-05 15:04:20 +0100

    Adding a navbar menu for the project

--- main.py
+++ main.py
@@ -1,10 +1,24 @@
 #https://davidsiltroy-python-big-data-image-classification-main-op94jq.streamlit.app/
 import streamlit as st
+from streamlit_option_menu import option_menu
 
-st.header('Thomas More - Big Data Team Assigment: Image Classification')
 
-st.write("Here is going to be an Epic/Awesome image classificatior program!")
 
-body = st.container()
-body.write("Coming soon.... (in Winter)")
+
+#1. as slidebar menu
+with st.sidebar:
+    selected = option_menu(
+        menu_title= "Team 1: JAD Solutions",
+        options = ["Notebook", "IDE", "About us"]
+    )
+
+if selected == "Notebook":
+    st.header('Thomas More - Big Data Team Assigment: Image Classification')
+
+if selected == "IDE":
+    st.write("Here is going to be an Epic/Awesome image classificatior program!")
+
+if selected == "About us":
+    body = st.container()
+    body.write("Coming soon.... (in Winter)")
 
