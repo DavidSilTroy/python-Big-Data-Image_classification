1c621fe02ed6b5d7bb56d873c515e4c4a7d2af16
David Silva Troya <davidsiltroy2008@hotmail.com>
2022-12-11 16:35:32 +0100

    API running and working just fine

--- tanks_api.py
+++ tanks_api.py
@@ -11,32 +11,27 @@ from fastai.vision.all import *
 
 app = FastAPI()
 
-@app.get("/")
+@app.get("/") #Redirecting to the fastai interface to check the methods
 async def root():
     response = RedirectResponse(url='/docs#/default/create_upload_file_uploadfile__post')
     return response
 
-@app.get("/api")
+@app.get("/api") #Just friendly greetings
 async def root():
     return {"message": "Hello! you have to send a picture of your tank xd"}
 
 
 @app.post("/uploadfile/")
 async def create_upload_file(file: UploadFile):
-
-    image = read_image(await file.read())
-    return predict_tank(image)
+    image = read_image(await file.read()) #transforming the file in a fastai image
+    return predict_tank(image) #Getting the prediction
 
 
 
 def read_image(file: bytes) -> PILImage:
     img = Image.open(BytesIO(file))
     fastimg = PILImage.create(np.array(img.convert('RGB')))
-
     return fastimg
-
-def is_hotdog(x):
-    return "frankfurter" in x or "hotdog" in x or "chili-dog" in x
 
 def predict_tank(image) -> Dict:
     model_name='tanks.pkl' #in case the model is moved to a folder -> 'folder/tanks.pkl'
