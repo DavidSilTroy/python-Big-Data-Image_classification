5f37fe1f37b176c60526447894acce774d29eac0
Axel Van Gestel <r0784084@student.thomasmore.be>
2022-12-14 15:40:00 +0100

    Added probability bar charts & Added EDA overview

--- googledrive.py
+++ googledrive.py
@@ -2,13 +2,14 @@ import streamlit as st
 import pathlib
 from PIL import Image
 from io import BytesIO, StringIO
+import pandas as pd
+import numpy as np
 
 from fastbook import *
 from fastai.vision.widgets import *
 from fastai.vision.all import *
 import io
 import requests
-# import gdown
 
 
 # Get the pkl file from Google Drive and cache it
@@ -22,22 +23,29 @@ def get_model():
     return model
 
 
+@st.experimental_memo
+def get_categories(_model):
+    return _model.dls.vocab
+
+
 def googledrive():
     st.header('Thomas More - Big Data Team Assigment: Image Classification')
     st.subheader('War Tanks Classification - Google Drive storage')
 
-    # id = "1-apihmCjwU5QnQburu_70UAUMDs0l1RP"
-    # output = "export.pkl"
-    # gdown.download(id=id, output=output, quiet=False)
-
     model_pkl = get_model()
     model = load_learner(model_pkl)
 
-    image = st.file_uploader("Upload file", type=["png", "jpg", "jpeg"])
+    if st.button("Clear cache and redownload model", help="Useful when the cached model is older than the one on Google Drive"):
+        # Clear get_model's memoized values:
+        get_model.clear()
+        model_pkl = get_model()
+        model = load_learner(model_pkl)
+
+    image = st.file_uploader("Upload file", type=["png", "jpg", "jpeg", "webp"])
     show_image = st.empty()
 
     if not image:
-        show_image.info("Choose a file to upload, only type: png, jpg, jpeg ")
+        show_image.info("Choose a file to upload, only type: png, jpg, jpeg, webp ")
         return
     else:
         image = PILImage.create((image))
@@ -45,8 +53,10 @@ def googledrive():
 
     if st.button('Classify'):
         pred, pred_idx, probs = model.predict(image)
-        st.write(f'Prediction: {pred}; Probability: {probs[pred_idx]:.04f}')
-        # st.write(f'Prediction: {model.predict(image)}')
+        st.write(f'Prediction: {pred}; Probability: {probs[pred_idx]*100:.02f}%')
+
+        chart_data = pd.DataFrame({'Probability (%)': probs*100}, index=get_categories(model))
+        st.bar_chart(chart_data)
     else:
         st.write(f'Click the button to classify')
 
