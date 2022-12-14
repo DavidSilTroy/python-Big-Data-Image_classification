797e393871d84b83e8d2a40444313b24b0ee5490
Axel Van Gestel <r0784084@student.thomasmore.be>
2022-12-14 21:04:31 +0100

    Pushed cleaned up formatting files this time

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-#https://davidsiltroy-python-big-data-image-classification-main-op94jq.streamlit.app/
+# https://davidsiltroy-python-big-data-image-classification-main-zrtmm1.streamlit.app/
 import streamlit as st
 from navbar import *
 
-
-if __name__=='__main__':
+if __name__ == '__main__':
     navbar()
